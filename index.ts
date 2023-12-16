c0e5ee514be54d0ad57abbb84c7ef676263f2f8e
stephydlb <111629805+stephydlb@users.noreply.github.com>
2023-12-16 13:10:27 +0200

    Add files via upload
    adding files

---
+++ index.ts
@@ -0,0 +1,14 @@
+//Numbers
+let myNum = 10
+let annotherNumm: number = 20;
+
+myNum = 12;
+myNum = '12';
+
+annotherNumm = 30;
+annotherNumm = false;
+
+//strings
+
+let mystr: string = 'hello'
+let annotherstr = 'wolrd'
